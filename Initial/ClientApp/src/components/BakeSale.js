a517f919f71fa5ac1047b2aa106d7b319202c037
Henri Hummal <hehumm@taltech.ee>
2020-08-08 21:02:30 +0300

    Dropped Customer class as it was unnecessary. Created a rough estimate of the UI

--- Initial/ClientApp/src/components/BakeSale.js
+++ Initial/ClientApp/src/components/BakeSale.js
@@ -18,20 +18,35 @@ export class BakeSale extends Component {
     render() {
         return (
             <div>
-            <h1>Bake Sale</h1>
+                <h1>Bake Sale</h1>
 
-            <p>Welcome to our bake sale. Thanks for supporting us!</p>
+                <p>Welcome to our bake sale. Thanks for supporting us!</p>
 
-            <p><strong>Currently on sale:</strong></p> 
+                <p><strong>Currently on sale:</strong></p> 
 
-                <div class="form-group">
-                    <label for="itemSelection">Product selection</label>
-                    <input type="text" class="form-control" id="itemSelection" aria-describedby="itemsToPurchase" placeholder="Select items for purchasing"></input>
-                    <small id="itemsToPurchase" class="form-text text-muted">Example format for 2 brownies and 1 muffin: B2,M1</small>
-
-                    <label for="money">Payment</label>
-                    <input type="number" class="form-control" id="money" placeholder="Insert the appropriate amount of money"></input>
-                </div> 
+                    <div class="row">
+                        <div class="col-md2">
+                            <label for="itemSelection">Product selection</label>
+                            <input type="text" class="form-control" id="itemSelection" aria-describedby="itemsToPurchase" placeholder="Select items for purchasing"></input>
+                            <small id="itemsToPurchase" class="form-text text-muted">Example format for 2 brownies and 1 muffin: B2,M1</small>
+                        </div>
+                        <div class="col-md2 offset-2">
+                            <label for="money">Payment</label>
+                            <input type="number" class="form-control" id="money" aria-describedby="money"></input>
+                            <small id="money" class="form-text text-muted">Insert the appropriate amount of money</small> 
+                        </div>
+                    </div> 
+                    <div class="row">
+                        <div class="col-3">
+                            <p>Your chosen items:</p> 
+                        </div>
+                        <div class="col-2 offset-2">
+                            <button className="btn btn-primary">Checkout</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                    
+                    </div>
             </div>
     );
 }
